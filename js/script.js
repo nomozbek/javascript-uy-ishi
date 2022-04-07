b9a5cfd51d5e7df1598e115ec41833eb805bc416
nomozbek <suyunovnomozbek472@gmail.com>
2022-04-07 11:01:39 +0500

    git init.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var welcome = alert('Hush Kelibsiz');
+var nameOfUser = prompt('Ismingiz sharifingiz');
+var surnameOfUser = prompt('Familiyangiz');
+var ageOfUser = prompt('Yoshingiz nechida?');
+var nationalityOfUser = prompt('Millatingiz?');
+var positionOfUser = prompt('Oldin qayerda ishlagansiz?');
+
+
+
+
+console.log(`nameOfUser,surnameOfUser, ageOfUser, nationalityOfUser, positionOfUser`);
