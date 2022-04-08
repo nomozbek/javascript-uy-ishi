6c4b4a11e1d98b60491a6033dd7707ac9ce53996
nomozbek <suyunovnomozbek472@gmail.com>
2022-04-08 23:19:53 +0500

    used if else,to do better

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@ var ageOfUser = prompt('Yoshingiz nechida?');
 var nationalityOfUser = prompt('Millatingiz?');
 var positionOfUser = prompt('Oldin qayerda ishlagansiz?');
 
+var question = confirm('Savollarga boshqatdan javob bermoqchimisiz?')
+
+if(question){
+  alert('Unda saytni bir "Refresh" qilib yuboring ;)')
+}
+else{
+  alert('Siz "Отмена" tugmasini bosdingiz')
+}
 
 
-
-console.log(
-  `Ism: ${nameOfUser}\n
+console.log(`Ism: ${nameOfUser}\n
   Familiya:${surnameOfUser}\n
   Yosh:${ageOfUser}\n
   Millati:${nationalityOfUser}\n
   Ish joyi:${positionOfUser}`
 );
+
+
